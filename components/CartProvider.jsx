0327ef5216652b770e892fc14d06f1b339968a3c
Tomoyuki Yoshikawa <yosshyjungle@gmail.com>
2024-11-20 09:30:03 +0900

    localhost -> https://bikebizz.vercel.app/

--- components/CartProvider.jsx
+++ components/CartProvider.jsx
@@ -8,8 +8,8 @@ const CartProvider = ({children}) => {
      mode="payment"
      cartMode="client-only"
      stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-     successUrl="http://localhost:3000/stripe/success"
-     cancelUrl="http://localhost:3000/stripe/error"
+     successUrl="https://bikebizz.vercel.app/stripe/success"
+     cancelUrl="https://bikebizz.vercel.app/stripe/error"
      language="en-US"
      currency="USD"
      billingAddressCollection={true}
